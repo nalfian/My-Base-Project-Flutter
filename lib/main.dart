939de960a566ca36ef949b2b7b30b03625af6b19
Nur Alfian Julianda <alfian@aqi.co.id>
2022-01-19 21:39:25 +0700

    web with frame

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,12 @@
-import 'dart:html';
-
 import 'package:base_project/config/theme_manager.dart';
 import 'package:base_project/screen/splash_screen/splash_screen.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_web_frame/flutter_web_frame.dart';
 import 'package:provider/provider.dart';
 import 'config/routes.dart' as router;
-import 'package:flutter/foundation.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,34 +21,28 @@ void main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Consumer<ThemeManager>(
-        builder: (context, theme, child) => ScreenUtilInit(
-              designSize: checkIsComputer() ? Size(1600, 900) : Size(375, 812),
-              builder: () => MaterialApp(
-                theme: theme.getTheme(),
-                builder: (context, widget) {
-                  return MediaQuery(
-                    data: MediaQuery.of(context).copyWith(textScaleFactor: 1),
-                    child: widget,
-                  );
-                },
-                debugShowCheckedModeBanner: false,
-                initialRoute: SplashScreen.routeName,
-                onGenerateRoute: (settings) => router.Router.generateRoute(settings),
-              ),
-            ));
-  }
-
-  bool checkIsComputer() {
-    if (kIsWeb) return getOSInsideWeb() == 'computer';
-    return false;
-  }
-
-  String getOSInsideWeb() {
-    final userAgent = window.navigator.userAgent.toString().toLowerCase();
-    if (userAgent.contains("iphone")) return "ios";
-    if (userAgent.contains("ipad")) return "ios";
-    if (userAgent.contains("android")) return "Android";
-    return "computer";
+    return FlutterWebFrame(
+      builder: (context) {
+        return Consumer<ThemeManager>(
+            builder: (context, theme, child) => ScreenUtilInit(
+                  designSize: Size(475, 812),
+                  builder: () => MaterialApp(
+                    theme: theme.getTheme(),
+                    builder: (context, widget) {
+                      return MediaQuery(
+                        data: MediaQuery.of(context).copyWith(textScaleFactor: 1),
+                        child: widget,
+                      );
+                    },
+                    debugShowCheckedModeBanner: false,
+                    initialRoute: SplashScreen.routeName,
+                    onGenerateRoute: (settings) => router.Router.generateRoute(settings),
+                  ),
+                ));
+      },
+      maximumSize: Size(475, 812),
+      enabled: kIsWeb,
+      backgroundColor: Colors.grey,
+    );
   }
 }
